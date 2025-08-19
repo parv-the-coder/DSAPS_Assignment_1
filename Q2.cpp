d04029c2543d2c27c108e673c95fb1796f9beda6
parv-the-coder <parvthecoder@gmail.com>
2025-08-19 17:16:31 +0530

    Completed AutoComplete

--- Q2.cpp
+++ Q2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct trinode
@@ -65,7 +66,52 @@ bool scan(trinode *root, string &word)
     return node->end;
 }
 
+// depth first search to get all words from given node
+void dfs(trinode *node, string str, vector<string> &ans)
+{
 
+    if (!node) // checking is node is null nothing is there to return
+    {
+        return;
+    }
+
+    if (node->end)
+    {
+        ans.push_back(str);
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (node->leaves[i]) // going further
+        {
+            dfs(node->leaves[i], str + char('a' + i), ans); // all possible words with given prefix
+        }
+    }
+}
+
+// auto complete function
+vector<string> autocomplete(trinode *root, string &pre)
+{
+    trinode *node = root;
+
+    // finding prefix in tri
+    for (char c : pre)
+    {
+        int temp = c - 'a';
+        if (!node->leaves[temp])
+        {
+            return {}; // if pre path doesnot exist
+        }
+
+        node = node->leaves[temp]; // moving till we reach at end of pre
+    }
+
+    vector<string> ans;
+    dfs(node, pre, ans);          // to get all permutation
+    sort(ans.begin(), ans.end()); // sort in lexico order
+
+    return ans;
+}
 
 int main()
 {
@@ -92,6 +138,16 @@ int main()
         if (flag == 1)
         {
             cout << scan(root, query) << "\n";
+        }
+        else if (flag == 2)
+        {
+            vector<string> words = autocomplete(root, query);
+            cout << words.size() << endl; // total no. of words found
+
+            for (string &str : words) // for eeach word in vector
+            {
+                cout << str << endl;
+            }
         }
     }
 }
